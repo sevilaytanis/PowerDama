efbcac1e3ed2e629c58c77a27076b0721cd4eb66
Kerem Can <ce.cankerem@outlook.com>
2021-08-18 20:16:04 +0300

    login düzenleme

--- PowerDama.MVC/Controllers/LoginController.cs
+++ PowerDama.MVC/Controllers/LoginController.cs
@@ -4,12 +4,30 @@ namespace PowerDama.MVC.Controllers
 {
     public class LoginController : Controller
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public LoginController()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [Route("giris")]
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         [HttpPost]        
         public IActionResult Index(string username, string password)
         {
